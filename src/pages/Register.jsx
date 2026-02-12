6766489ecab9757320e9803f8bf2a8f16d3ac281
murthehelp24 <attachaijumpahom@gmail.com>
2026-02-12 20:00:16 +0700

    validate feature

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,25 +2,39 @@ import axios from 'axios';
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
+import { registerValidator } from '../validators/register.validator';
+
+
+const inpStyle = 'border border-gray-500 my-1 pl-2 rounded-lg'
 
 function Register() {
-  const [formData , setFormData] = useState({
+  const [formData, setFormData] = useState({
     username: "",
     password: "",
     email: "",
     phon: "",
   })
 
+  const [error, setError] = useState(null)
+
   const navigate = useNavigate()
 
-  const hdlChange =(e)=> {
-    const {name, value } = e.target;
-    setFormData((prev)=> ({ ...prev, [name]: value}))
-    console.log(name, value)
+  const hdlChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }))
   }
 
-  const hdlSubmit = async (e)=> {
+  const hdlSubmit = async (e) => {
     e.preventDefault()
+    setError({}) //setError อีกรอบเพื่อเคลียร์ state ใหม่ให้เป็น obj ว่าง
+    const result = registerValidator.safeParse(formData)
+    if(!result.success) {
+      const { fieldErrors } = result.error.flatten()
+      console.log(fieldErrors)
+      setError(fieldErrors)
+      return
+    }
+
     try {
       const res = await axios.post(
         "https://jsonplaceholder.typicode.com/posts",
@@ -33,22 +47,29 @@ function Register() {
       console.log("เกิดข้อผิดพลาด")
       toast.error("ลงทะเบียนผิดพลาด")
     }
-      
-  }
-
+  }   
+  
+  console.log("error", error)
   return (
     <div className='flex justify-center items-center h-screen bg-gray-200'>
       <form onSubmit={hdlSubmit} className='flex flex-col w-full max-w-md p-6 bg-gray-300 rounded-xl shadow-2xl border border-gray-400'>
         <h2 className='text-center text-2xl text-gray-800'>Create Account</h2>
-          <label></label>username :
-          <input name='username' type="text" onChange={hdlChange} className='border border-gray-500 my-1 pl-2 rounded-lg' />
-          <label></label>password :
-          <input name='password' type="password" onChange={hdlChange} className='border border-gray-500 my-1 pl-2 rounded-lg' />
-          <label></label>email :
-          <input name='email' type="email" onChange={hdlChange} className='border border-gray-500 my-1 pl-2 rounded-lg' />
-          <label></label>phon :
-          <input name='phon' type="text" onChange={hdlChange} className='border border-gray-500 my-1 pl-2 rounded-lg' />
-          <button className='my-4 p-1 text-white bg-gray-600 rounded-2xl hover:bg-gray-700'>Register</button>
+        <label></label>username :
+        <input name='username' type="text" placeholder='username' value={formData.username} onChange={hdlChange} className={inpStyle} />
+        {error?.username && <p className='text-red-500'>{error?.username[0]}</p>}
+
+        <label></label>password :
+        <input name='password' type="password" placeholder='password' onChange={hdlChange} className={inpStyle} />
+        {error?.password && <p className='text-red-500'>{error?.password[0]}</p>}
+
+        <label></label>email :
+        <input name='email' type="text" placeholder='e-mail@gmail.com' onChange={hdlChange} className={inpStyle} />
+        {error?.email && <p className='text-red-500'>{error?.email[0]}</p>}
+
+        <label></label>phone :
+        <input name='phone' type="text" placeholder='088-xxxxxxx' onChange={hdlChange} className={inpStyle} />
+        {error?.phone && <p className='text-red-500'>{error?.phone[0]}</p>}
+        <button className='my-4 p-1 text-white bg-gray-600 rounded-2xl hover:bg-gray-700'>Register</button>
       </form>
     </div>
   )
